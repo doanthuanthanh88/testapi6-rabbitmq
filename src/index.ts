24c98e4a64ff1236ba278f28a09e24cefff809f4
Đoàn Thuận Thành <doanthuanthanh88@gmail.com>
2021-05-08 17:07:50 +0700

    Export to global

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,66 @@
-// export { RMQRoutingKeyConsumer, RMQRoutingKeyPublisher } from "./RoutingKey"
+export { RMQRoutingKeyConsumer, RMQRoutingKeyPublisher } from "./RoutingKey"
 
-import { RMQRoutingKeyConsumer } from "./RoutingKey"
+// import { RMQRoutingKeyConsumer } from "./RoutingKey"
 
-async function main() {
-  // setTimeout(async () => {
-  const m = new RMQRoutingKeyConsumer({
-    title: 'Test consumer',
-    connection: 'amqp://user:password@localhost',
-    exchanges: [{
-      name: 'thanh',
-      type: 'direct',
-      exchangeOpts: {
-        durable: true
-      },
-      targets: [
-        {
-          queue: 'thanh-test-queue',
-          routingKey: 'test',
-          consumeOpts: {
-            exclusive: false
-          },
-          autoAck: true
-        }, {
-          queue: 'thanh-test1-queue',
-          routingKey: 'test1',
-          consumeOpts: {
-            exclusive: false
-          },
-          autoAck: true
-        }
-      ]
-    }]
-  } as RMQRoutingKeyConsumer)
-  m.prepare()
-  await m.beforeExec()
-  await m.exec()
-  console.log('done')
-  // })
-  // const m = new RMQRoutingKeyPublisher({
-  //   title: 'Test publisher',
-  //   connection: 'amqp://user:password@localhost',
-  //   exchanges: [{
-  //     name: 'thanh',
-  //     type: 'direct',
-  //     exchangeOpts: {
-  //       durable: true
-  //     },
-  //     targets: [
-  //       {
-  //         routingKey: 'test',
-  //         data: { age: 123 }
-  //       },
-  //       {
-  //         routingKey: 'test1',
-  //         data: { age: 123666 }
-  //       }
-  //     ]
-  //   }]
-  // } as RMQRoutingKeyPublisher)
-  // m.prepare()
-  // await m.beforeExec()
-  // await m.exec()
-}
+// async function main() {
+//   // setTimeout(async () => {
+//   const m = new RMQRoutingKeyConsumer({
+//     title: 'Test consumer',
+//     connection: 'amqp://user:password@localhost',
+//     exchanges: [{
+//       name: 'thanh',
+//       type: 'direct',
+//       exchangeOpts: {
+//         durable: true
+//       },
+//       targets: [
+//         {
+//           queue: 'thanh-test-queue',
+//           routingKey: 'test',
+//           consumeOpts: {
+//             exclusive: false
+//           },
+//           autoAck: true
+//         }, {
+//           queue: 'thanh-test1-queue',
+//           routingKey: 'test1',
+//           consumeOpts: {
+//             exclusive: false
+//           },
+//           autoAck: true
+//         }
+//       ]
+//     }]
+//   } as RMQRoutingKeyConsumer)
+//   m.prepare()
+//   await m.beforeExec()
+//   await m.exec()
+//   console.log('done')
+//   // })
+//   // const m = new RMQRoutingKeyPublisher({
+//   //   title: 'Test publisher',
+//   //   connection: 'amqp://user:password@localhost',
+//   //   exchanges: [{
+//   //     name: 'thanh',
+//   //     type: 'direct',
+//   //     exchangeOpts: {
+//   //       durable: true
+//   //     },
+//   //     targets: [
+//   //       {
+//   //         routingKey: 'test',
+//   //         data: { age: 123 }
+//   //       },
+//   //       {
+//   //         routingKey: 'test1',
+//   //         data: { age: 123666 }
+//   //       }
+//   //     ]
+//   //   }]
+//   // } as RMQRoutingKeyPublisher)
+//   // m.prepare()
+//   // await m.beforeExec()
+//   // await m.exec()
+// }
 
-main()
+// main()
